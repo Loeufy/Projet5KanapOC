9cbdbb672b8420fb42bd157479f5b4a2bf9799ff
Enzo Loeufy <xizohd06@gmail.com>
2022-12-02 16:13:38 +0100

    Commit02122

---
+++ front/js/script.js
@@ -0,0 +1,26 @@
+fetch('http://localhost:3000/api/products')
+  .then((response) => response.json())
+  .then((Produits) => {
+    console.table(Produits);
+    Kanaps(Produits);
+  })
+
+  .catch((erreur) => {
+    document.querySelector(".titles").innerHTML = "<h1>erreur 404</h1>";
+    console.log("erreur 404" + erreur);
+  });
+
+  function Kanaps(index) {
+    let zoneArticle = document.querySelector("#items");
+    for (let article of index) {
+      zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
+      <article>
+        <img src="${article.imageUrl}" alt="${article.altTxt}">
+        <h3 class="productName">${article.name}</h3>
+        <p class="productDescription">${article.description}</p>
+      </article>
+    </a>`;
+    }
+  }
+
+
